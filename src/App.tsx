88ff2eeda02f3fbd097be55c22a1903863cf6acd
Francisco <franciscoadolfo@gmail.com>
2026-06-14 23:15:21 -0400

    type: self-host Satoshi, drop Fontshare CDN dependency

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import '@fontsource/inter/400.css';
 import '@fontsource/inter/500.css';
 import '@fontsource/inter/600.css';
 import '@fontsource/inter/700.css';
+import './styles/fonts/satoshi.css';
 import './App.module.scss';
 import { removeStructuredData, setPageSeo, setStructuredData } from './utilities/seo';
 
